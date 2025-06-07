d7890dffe1ff6844697139013f4f55a625ef26de
Devin Catuns <devincatuns1@gmail.com>
2025-06-07 18:32:23 -0400

    feat: copy deps generator added

--- next-feature/src/generators/copy-deps/copy-deps.ts
+++ next-feature/src/generators/copy-deps/copy-deps.ts
@@ -1,25 +1,29 @@
-import { logger } from '@nx/devkit';
+import { readJson } from '@nx/devkit';
 import { addDependenciesToPackageJson } from '@nx/devkit';
 import { Tree } from '@nx/devkit';
-import { readPackageJson } from 'nx/src/project-graph/file-utils';
+import * as path from 'node:path';
 import { CopyDepsGeneratorSchema } from './schema';
 
 export async function copyDepsGenerator(
   tree: Tree,
   options: CopyDepsGeneratorSchema
 ) {
-  const packageJson = readPackageJson(options.directory);
-
-  logger.debug(packageJson);
+  const packageJson = readJson(
+    tree,
+    path.join(options.directory, 'package.json')
+  );
 
   if (!packageJson) {
     throw new Error('No package.json found');
   }
 
-  const dependencies =
-    packageJson['dependencies'] ?? ({} as Record<string, string>);
-  const devDependencies =
-    packageJson['devDependencies'] ?? ({} as Record<string, string>);
+  const dependencies: Record<string, string> = {
+    ...packageJson['dependencies'],
+  };
+  const devDependencies: Record<string, string> = {
+    ...packageJson['devDependencies'],
+  };
+  // logger.debug({ dependencies, devDependencies });
 
   return addDependenciesToPackageJson(
     tree,

1778d1b6251ae5183ab6f636a8e4689f2757139b
Devin Catuns <devincatuns1@gmail.com>
2025-07-03 10:55:20 -0400

    feature: init utils generator

---
+++ next-feature/src/generators/utils/utils.ts
@@ -0,0 +1,25 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  Tree,
+} from '@nx/devkit';
+import * as path from 'path';
+import { UtilsGeneratorSchema } from './schema';
+
+export async function utilsGenerator(
+  tree: Tree,
+  options: UtilsGeneratorSchema
+) {
+  const projectRoot = `libs/${options.name}`;
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'library',
+    sourceRoot: `${projectRoot}/src`,
+    targets: {},
+  });
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  await formatFiles(tree);
+}
+
+export default utilsGenerator;
